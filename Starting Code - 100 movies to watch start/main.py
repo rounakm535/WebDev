b7f6239716d8f1b045f1d8f42f4c92dcac5e91cc
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-10-25 14:10:46 +0530

    .....

---
+++ Starting Code - 100 movies to watch start/main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+web_page = response.text
+
+print(web_page)
